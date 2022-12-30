c9060b0c111a9a09fe08bc4cb4fe5623451d527c
T'Vedt Lazenby <tvedt@10downproductions.com>
2022-12-29 19:39:52 -0500

    front end & back end should be connected

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.get("/getData" , (req,res)=>{
+    res.send("test message");
+});
+
+app.listen(5000, ()=> console.log("app is running")); 
